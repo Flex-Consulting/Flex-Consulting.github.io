006255d53f663b5ad46db1a02eddb7de843af959
MatthewSzurkowski <mszurkow@gmail.com>
2023-01-18 11:00:21 -0500

    Footer fixes, main blob fixes.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,24 +13,25 @@ function Footer(){
         <div id="main-footer">
             <div id="media">
                 <div className="footer-sec">
-                    <img src="./Flex-Logo-White.png" width="100" height="100" className="d-inline-block align-top" alt="Flex Consulting Logo"/>
+                    <img src="./flex-footer.png" width="50" height="50" className="d-inline-block align-top image-space" alt="Flex Consulting Logo"/>
                     <div id="organize-name">
                         <p>Flex<br/>Consulting<br/>Services</p>
                     </div>
                 </div>
                 <div className='footer-sec'>
-                    <img src="./linkedin-icon-18-256.png" width="50" height="50" className="d-inline-block align-top" alt="Flex Consulting Logo"/>
+                    <img src="./linkedin-icon-18-256.png" width="50" height="50" className="d-inline-block align-top image-space" alt="Flex Consulting Logo"/>
                     <div id="organize-name">
                         <p>LinkedIn</p>
                     </div>
                 </div>
             </div>
             {/* Locations */}
+            
             <div className="footer-sub">
                 <h4>Locations</h4>
                 <div>
                     {Locations["locations"].map(function(object, i){
-                        return <p key={i}>{object}</p>;
+                        return <p className="footer-text-items" key={i}>{object}</p>;
                     })}
                 </div>
             </div>
@@ -38,7 +39,7 @@ function Footer(){
                 <h4>Pages</h4>
                 <div>
                     {Pages["pages"].map(function(object, i){
-                        return <p key={i}>{object}</p>;
+                        return <p className="footer-text-items"key={i}>{object}</p>;
                     })}
                 </div>
             </div>
