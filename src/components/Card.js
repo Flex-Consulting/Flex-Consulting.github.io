cccbd8b12f9b5fe1e6231087cc9e7f2bf73631b1
socs <william_fitzjohn@icloud.com>
2023-01-20 13:51:24 -0500

    Fixed css card issue. Closes #3

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,8 +5,10 @@ function Card(props){
     return(
         <div className="card" style={{background: `-webkit-linear-gradient(${props.color1}, ${props.color2})`}}>
             <p className="service-name" style={{color: `${props.nameLink}`}}>{props.info["name"]}</p>
-            <h1 className="service-desc">{props.info["brief-desc"]}</h1>
-            <a className="card-link" href={props.info["link-dest"]} style={{color: `${props.nameLink}`}}>{props.info["link-text"]}</a>
+            <div>
+                <h1 className="service-desc">{props.info["brief-desc"]}</h1>
+                <a className="card-link" href={props.info["link-dest"]} style={{color: `${props.nameLink}`}}>{props.info["link-text"]}</a>
+            </div>
         </div>
     );
 }
